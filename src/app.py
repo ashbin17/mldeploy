5b3b28f6a5421e615eba5b0cdc70e9cf06a751a2
Gautham Prakash <gauthamp10@gmail.com>
2020-09-28 12:52:38 +0530

    added 404

--- src/app.py
+++ src/app.py
@@ -20,3 +20,8 @@ def get_prediction():
         prediction = str(round(float(prediction), 2))
         result = {"Predicted Weight": prediction + "Kg"}
         return render_template('index.html', data=result)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+   return render_template('404.html', title = '404'), 404
